4e36e16b4bf1a2fbdfb30ae9f2199f291f43d36e
Stric Roberts <stric9r@gmail.com>
2026-05-30 13:54:04 -0400

    Add generated project

---
+++ generated_project/silabs_efr32bg22_ble_soc_lcd1602_i2c_controller/GNU ARM v12.2.1 - Default/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS := /home/stric9r/git/silabs_efr32bg22_ble_soc_lcd1602_i2c_controller/generated_project/silabs_efr32bg22_ble_soc_lcd1602_i2c_controller/gecko_sdk_4.4.5/protocol/bluetooth/bgcommon/lib/libbgcommon_efr32xg22_gcc_release.a /home/stric9r/git/silabs_efr32bg22_ble_soc_lcd1602_i2c_controller/generated_project/silabs_efr32bg22_ble_soc_lcd1602_i2c_controller/gecko_sdk_4.4.5/protocol/bluetooth/bgstack/ll/lib/libbluetooth_controller_efr32xg22_gcc_release.a /home/stric9r/git/silabs_efr32bg22_ble_soc_lcd1602_i2c_controller/generated_project/silabs_efr32bg22_ble_soc_lcd1602_i2c_controller/gecko_sdk_4.4.5/protocol/bluetooth/lib/libbluetooth_host_efr32xg22_gcc_release.a /home/stric9r/git/silabs_efr32bg22_ble_soc_lcd1602_i2c_controller/generated_project/silabs_efr32bg22_ble_soc_lcd1602_i2c_controller/gecko_sdk_4.4.5/platform/emdrv/nvm3/lib/libnvm3_CM33_gcc.a /home/stric9r/git/silabs_efr32bg22_ble_soc_lcd1602_i2c_controller/generated_project/silabs_efr32bg22_ble_soc_lcd1602_i2c_controller/gecko_sdk_4.4.5/platform/radio/rail_lib/autogen/librail_release/librail_efr32xg22_gcc_release.a
+
+LIBS := -lgcc -lc -lm -lnosys
+
